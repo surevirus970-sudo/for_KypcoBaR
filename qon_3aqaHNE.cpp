b76047f478d850f02ac9975d66319adc8bb4ddc9
surevirus970-sudo <surevirus970@mail.ru>
2025-12-17 14:44:12 +0300

    Create qon_3aqaHNE.cpp

---
+++ qon_3aqaHNE.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Введите размер массива: ";
+    cin >> n;
+
+    vector<int> arr(n);
+    cout << "Введите элементы: ";
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+
+    int iz = 0;
+    for (int i = 0; i < n; ++i) {
+        if (arr[i] != 0) {
+            swap(arr[iz], arr[i]);
+            iz++;
+        }
+    }
+
+    cout << "Результат: ";
+    for (int i = 0; i < n; ++i) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
