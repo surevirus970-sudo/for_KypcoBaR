9f0d17dcba07fbe6fce39da1b3e0881d5f5edf83
surevirus970-sudo <surevirus970@mail.ru>
2025-12-17 15:03:15 +0300

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -1,1 +1,39 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Введите размер массива: ";
+    cin >> n;
+
+    vector<int> arr(n);
+    cout << "Введите эелементы массива: ";
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+
+    int iz = 0;
+
+    for (int i = 0; i < n; ++i) {
+        if (arr[i] != 0) {
+            arr[iz] = arr[i];
+            iz++;
+        }
+    }
+
+    while (iz < n) {
+        arr[iz] = 0;
+        iz++;
+    }
+
+    cout << "Результат: ";
+    for (int i = 0; i < n; ++i) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
 
